7b7604d8019b42d053b769743f928d1d6cd5307d
Shreyas Shah <s4005805@student.rmit.edu.au>
2024-05-06 19:08:57 +1000

    Remove config.js from tracking

--- express/src/database/config.js
+++
@@ -1,8 +0,0 @@
-// change to match selected data base
-module.exports = {
-  HOST: "rmit.australiaeast.cloudapp.azure.com",
-  USER: "s4005805_fsd_a2",
-  PASSWORD: "Sh~010101",
-  DB: "s4005805_fsd_a2",
-  DIALECT: "mysql"
-};
