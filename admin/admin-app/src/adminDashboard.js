d41186c47ce292fd69078ec0e0034bed61d674d4
Idris Aklan <s4003401@student.rmit.edu.au>
2024-05-29 13:51:14 +1000

    update

--- admin/admin-app/src/adminDashboard.js
+++ admin/admin-app/src/adminDashboard.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { Button, Container, Grid, Card, CardContent, Typography, Box, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, ThemeProvider, createTheme } from '@mui/material';
+import { Button, Container, Grid, Card, CardContent, Typography, Box, Dialog, DialogActions, DialogContent, DialogContentText,
+  DialogTitle, ThemeProvider, createTheme, TextField, FormControl, InputLabel, Select, MenuItem, FormControlLabel, Checkbox } from '@mui/material';
 import { BarChart, Bar, XAxis, YAxis, ResponsiveContainer, Tooltip, Legend } from 'recharts';
 import { useQuery, useMutation, useSubscription } from '@apollo/client';
 //Import 'date-fns' to format the dateCreated of the review as there is no 'Date' type in GraphQL
 import { format } from 'date-fns';
 import { GET_LATEST_REVIEWS, GET_ALL_ACTIVE_REVIEWS, SUBSCRIBE_TO_REVIEW_UPDATES, SUBSCRIBE_TO_REVIEW_DELETED, SUBSCRIBE_TO_REVIEW_FLAGGED, MUTATION_TO_REVIEW_FLAGGED, MUTATION_TO_REVIEW_DELETED, 
-MUTATION_TO_USER_STATUS, GET_USERS} from './apollo/definitions';
+MUTATION_TO_USER_STATUS, GET_USERS,  GET_PRODUCTS, CREATE_PRODUCT, UPDATE_PRODUCT, DELETE_PRODUCT} from './apollo/definitions';
 import { ToastContainer, toast } from 'react-toastify';
 import BadWordsFilter from 'bad-words';
 import profaneWords from 'profane-words';
@@ -19,6 +20,7 @@ badWordsFilter.addWords(...profaneWords);
 //Create functions for detecting inappropriate content
 const sentiment = new Sentiment();
 const profanityFilter = new ProfanityFilter();
+
 
 //Create a theme for styling the admin dashboard
 const theme = createTheme({
@@ -71,19 +73,34 @@ function AdminDashboard() {
     pollInterval: 3000, //Poll every 3 seconds
   });
   const { data: usersData } = useQuery(GET_USERS);
+  const { data: productsData, loading: productsLoading, refetch: refetchProducts } = useQuery(GET_PRODUCTS);
 
   //Mutations
   const [toggleUserStatus] = useMutation(MUTATION_TO_USER_STATUS);
   const [flagReview] = useMutation(MUTATION_TO_REVIEW_FLAGGED);
   const [deleteReview] = useMutation(MUTATION_TO_REVIEW_DELETED);
-
+  const [createProduct] = useMutation(CREATE_PRODUCT);
+  const [updateProduct] = useMutation(UPDATE_PRODUCT);
+  const [deleteProduct] = useMutation(DELETE_PRODUCT);
+  
   //States
   const [reviews, setReviews] = useState([]);
   const [modalOpen, setModalOpen] = useState(false);
   const [flagModalOpen, setFlagModalOpen] = useState(false); //State for flag confirmation modal
   const [selectedReview, setSelectedReview] = useState(null);
   const [averageRatings, setAverageRatings] = useState([]);
-
+  const [products, setProducts] = useState([]);
+  const [newProduct, setNewProduct] = useState({
+    name: '',
+    description: '',
+    title: '',
+    type: '',
+    price: 0,
+    isSpecial: false, // Add isSpecial attribute with default value
+    img: ''
+  });
+  const [editedProduct, setEditedProduct] = useState(null); // State for edited product
+  
   //Subscriptions 
   const { data: reviewUpdatedData } = useSubscription(SUBSCRIBE_TO_REVIEW_UPDATES);
   const { data: reviewDeletedData } = useSubscription(SUBSCRIBE_TO_REVIEW_DELETED);
@@ -217,6 +234,97 @@ function AdminDashboard() {
       toast.error("Failed to flag review.");
     }
   };
+  
+  // Effect for fetching products
+  useEffect(() => {
+    // console.log("the useEffect for getting products is called",productsData.products)
+    if (!productsLoading && productsData) {
+      setProducts(productsData.products);
+    }
+  }, [productsData, productsLoading]);
+
+  // Function to handle input change for new product form
+  const handleNewProductChange = (e) => {
+    const { name, value, type, checked } = e.target;
+    const val = type === 'checkbox' ? checked : value; // Handle checkbox differently
+    setNewProduct(prevState => ({
+      ...prevState,
+      [name]: val
+    }));
+  };
+  
+  const handleConfirmDelete = async () => {
+    try {
+      //Handle deleting reviews from the admin dashboard
+      const response = await deleteReview({ variables: { reviewID: selectedReview } });
+      if (response.data.deleteReview) {
+        const remainingReviews = reviews.filter(r => r.reviewID !== selectedReview);
+        setReviews(remainingReviews);
+        toast.error("Review deleted successfully!");
+        setModalOpen(false);
+        refetchActiveReviews(); //Refetch the active reviews for to update the graph in real time
+        refetchLatestReviews(); //Refetch the latest reviews
+      }
+    } catch (error) {
+      //Display an error message to indicate unsuccessful action
+      console.error("Error deleting review:", error);
+      toast.error("Failed to delete review.");
+    }
+  };
+
+  // Function to handle submission of new product
+  const handleNewProductSubmit = async () => {
+    try {
+      console.log("this is the new product", newProduct);
+      newProduct.price = parseFloat(newProduct.price);
+      newProduct.title = newProduct.name;
+
+      await createProduct({ variables: { newProduct } });
+      toast.success('New product added successfully!');
+      setNewProduct({
+        name: '',
+        description: '',
+        title: '',
+        type: '',
+        price: 0,
+        isSpecial: false,
+        img: ''
+      });
+      refetchProducts();
+    } catch (error) {
+      console.error('Error adding new product:', error);
+      toast.error('Failed to add new product.');
+    }
+  };
+
+  const handleEditProduct = (product) => {
+    setEditedProduct(product);
+  };
+
+  const handleUpdateProduct = async () => {
+    try {
+      await updateProduct({ variables: { ...editedProduct } });
+      toast.success('Product updated successfully!');
+      setEditedProduct(null);
+      refetchProducts();
+    } catch (error) {
+      console.error('Error updating product:', error);
+      toast.error('Failed to update product.');
+    }
+  };
+
+
+  // Function to handle deletion of a product
+  const handleDeleteProduct = async (productID) => {
+    try {
+      await deleteProduct({ variables: { productID } });
+      toast.success('Product deleted successfully!');
+      refetchProducts();
+    } catch (error) {
+      console.error('Error deleting product:', error);
+      toast.error('Failed to delete product.');
+    }
+  };
 
 return (
   <ThemeProvider theme={theme}>
@@ -334,22 +442,216 @@ return (
         </DialogActions>
       </Dialog>
 
-      {/* Flag Confirmation Dialog */}
-    <Dialog open={flagModalOpen} onClose={() => setFlagModalOpen(false)}>
-    <DialogTitle>Confirm Flagging</DialogTitle>
-    <DialogContent>
-    <DialogContentText>
-      Are you sure you want to flag this review as inappropriate? This action cannot be undone.
-    </DialogContentText>
-    </DialogContent>
-    <DialogActions>
-    <Button onClick={() => setFlagModalOpen(false)}>Cancel</Button>
-    <Button onClick={handleFlagReview} color="primary">Flag</Button>
-    </DialogActions>
-    </Dialog>
-    </Container>
+        {/* Flag Confirmation Dialog */}
+        <Dialog open={flagModalOpen} onClose={() => setFlagModalOpen(false)}>
+          <DialogTitle>Confirm Flagging</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              Are you sure you want to flag this review as inappropriate? This action cannot be undone.
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={() => setFlagModalOpen(false)}>Cancel</Button>
+            <Button onClick={handleFlagReview} color="primary">Flag</Button>
+          </DialogActions>
+        </Dialog>
+
+                {/* Deletion Confirmation Dialog */}
+                <Dialog open={modalOpen} onClose={() => setModalOpen(false)}>
+          <DialogTitle>Confirm Deletion</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              Are you sure you want to delete this review?
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={() => setModalOpen(false)}>Cancel</Button>
+            <Button onClick={handleConfirmDelete} color="error">Delete</Button>
+          </DialogActions>
+        </Dialog>
+  
+        {/* Products Card */}
+        <Grid item xs={12} md={6}>
+          <Card sx={{ p: 3 }}>
+            <CardContent>
+              <Typography variant="h5" gutterBottom>Products</Typography>
+              <Grid container spacing={2}>
+                {products.map((product) => (
+                  <Grid key={product.productID} item xs={12}>
+                    <Card sx={{ p: 2 }}>
+                      <CardContent>
+                        <Typography variant="h6">{product.name}</Typography>
+                        <Typography>{product.description}</Typography>
+                        <Typography variant="body2" sx={{ mt: 1 }}>Type: {product.type}</Typography>
+                        <Typography variant="body2">Price: ${product.price.toFixed(2)}</Typography>
+                        <Button onClick={() => handleEditProduct(product)} variant="outlined" color="primary" sx={{ mt: 1, mr: 1 }}>Edit</Button>
+                        <Button onClick={() => handleDeleteProduct(product.productID)} variant="outlined" color="error" sx={{ mt: 1 }}>Delete</Button>
+                      </CardContent>
+                    </Card>
+                  </Grid>
+                ))}
+              </Grid>
+            </CardContent>
+          </Card>
+        </Grid>
+  
+        {/* Dialog for Editing Product */}
+        <Dialog open={!!editedProduct} onClose={() => setEditedProduct(null)}>
+          <DialogTitle>Edit Product</DialogTitle>
+          <DialogContent>
+            <DialogContentText>Make changes to the product details:</DialogContentText>
+            <TextField
+              autoFocus
+              margin="dense"
+              id="name"
+              label="Name"
+              type="text"
+              fullWidth
+              value={editedProduct?.name || ''}
+              onChange={(e) => setEditedProduct({ ...editedProduct, name: e.target.value })}
+            />
+            <TextField
+              margin="dense"
+              id="description"
+              label="Description"
+              type="text"
+              fullWidth
+              multiline
+              rows={4}
+              value={editedProduct?.description || ''}
+              onChange={(e) => setEditedProduct({ ...editedProduct, description: e.target.value })}
+            />
+
+            <FormControl fullWidth>
+              <InputLabel id="type-label">Type</InputLabel>
+              <Select
+                labelId="type-label"
+                id="type"
+                value={editedProduct?.type || ''}
+                onChange={(e) => setEditedProduct({ ...editedProduct, type: e.target.value })}
+              >
+                <MenuItem value="">Select Type</MenuItem>
+                <MenuItem value="Dairy & Eggs">Dairy & Eggs</MenuItem>
+                <MenuItem value="Fruits">Fruits</MenuItem>
+                <MenuItem value="Vegetables">Vegetables</MenuItem>
+                <MenuItem value="Bakery">Bakery</MenuItem>
+                <MenuItem value="Meat & Seafood">Meat & Seafood</MenuItem>
+                <MenuItem value="Grains & Pasta">Grains & Pasta</MenuItem>
+                <MenuItem value="Nuts & Seeds">Nuts & Seeds</MenuItem>
+              </Select>
+            </FormControl>
+            <TextField
+              margin="dense"
+              id="price"
+              label="Price"
+              type="number"
+              fullWidth
+              value={editedProduct?.price || ''}
+              onChange={(e) => setEditedProduct({ ...editedProduct, price: parseFloat(e.target.value) })}
+            />
+            <TextField
+              margin="dense"
+              id="img"
+              label="Image URL"
+              type="text"
+              fullWidth
+              value={editedProduct?.img || ''}
+              onChange={(e) => setEditedProduct({ ...editedProduct, img: e.target.value })}
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={() => setEditedProduct(null)} color="primary">
+              Cancel
+            </Button>
+            <Button onClick={handleUpdateProduct} color="primary">
+              Save
+            </Button>
+          </DialogActions>
+        </Dialog>
+  
+        {/* Add New Product Form */}
+        <Box sx={{ mt: 4 }}>
+          <Typography variant="h5" gutterBottom>Add New Product</Typography>
+          <Grid container spacing={2}>
+            <Grid item xs={12} md={6}>
+              <TextField
+                fullWidth
+                id="name"
+                name="name"
+                label="Name"
+                value={newProduct.name}
+                onChange={handleNewProductChange}
+              />
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <TextField
+                fullWidth
+                id="description"
+                name="description"
+                label="Description"
+                value={newProduct.description}
+                onChange={handleNewProductChange}
+              />
+            </Grid>
+            <Grid item xs={12} md={4}>
+            <FormControl fullWidth>
+              <InputLabel id="type-label">Type</InputLabel>
+              <Select
+                labelId="type-label"
+                id="type"
+                name="type" // Ensure the name attribute is set to 'type'
+                value={newProduct.type}
+                onChange={handleNewProductChange}
+              >
+                <MenuItem value="">Select Type</MenuItem>
+                <MenuItem value="Dairy & Eggs">Dairy & Eggs</MenuItem>
+                <MenuItem value="Fruits">Fruits</MenuItem>
+                <MenuItem value="Vegetables">Vegetables</MenuItem>
+                <MenuItem value="Bakery">Bakery</MenuItem>
+                <MenuItem value="Meat & Seafood">Meat & Seafood</MenuItem>
+                <MenuItem value="Grains & Pasta">Grains & Pasta</MenuItem>
+                <MenuItem value="Nuts & Seeds">Nuts & Seeds</MenuItem>
+              </Select>
+            </FormControl>
+            </Grid>
+            <Grid item xs={12} md={4}>
+              <TextField
+                fullWidth
+                id="price"
+                name="price"
+                label="Price"
+                type="number"
+                value={newProduct.price}
+                onChange={handleNewProductChange}
+              />
+            </Grid>
+            <Grid item xs={12} md={4}>
+              <TextField
+                fullWidth
+                id="img"
+                name="img"
+                label="Image URL"
+                value={newProduct.img}
+                onChange={handleNewProductChange}
+              />
+            </Grid>
+            <Grid item xs={12} md={4}>
+              <FormControlLabel
+                control={<Checkbox checked={newProduct.isSpecial} onChange={handleNewProductChange} name="isSpecial" />}
+                label="Special Product"
+              />
+            </Grid>
+
+            <Grid item xs={12}>
+              <Button variant="contained" color="primary" onClick={handleNewProductSubmit}>
+                Add Product
+              </Button>
+            </Grid>
+          </Grid>
+        </Box>
+      </Container>
     </ThemeProvider>
-);
-};
+  );
+}
 
 export default AdminDashboard;
